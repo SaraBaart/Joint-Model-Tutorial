0ca9e2b368d93d51f7452e7895f0b7e923360cef
Sara Baart <43849449+SaraBaart@users.noreply.github.com>
2021-04-08 16:32:30 +0200

    Update Analyses-JM-Tutorial.R

--- Analyses-JM-Tutorial.R
+++ Analyses-JM-Tutorial.R
@@ -1,6 +1,7 @@
 ###################################
 ##   Load necessary libraries    ##
 ###################################
+
 library(JMbayes)
 library(nlme)
 library(latticeExtra)
@@ -10,53 +11,55 @@ library(splines)
 ###############################
 ##     Data preparation      ##
 ###############################
-
+# Download the data from https://github.com/SaraBaart/Joint-Model-Tutorial
+# and load into R
+load("data.RData")
 # The data set needs to be in the long format
 
 # Variables in the data set:
 # id    = Patient identifier
-# y     = Longitudinal outcome
-# y2    = Second longitudinal outcome
+# y_a   = First longitudinal marker
+# y_b   = Second longitudinal marker
 # time  = Time of the longitudinal measurement
-# time2 = Variable "time" shifted one measurement. 
+# time2 = Variable "time" shifted forward one measurement 
 #         Necessary for interval censored model
 # Time  = Time of the event
-# event = Event identifier
+# event = Event indicator (1 if event happened, 0 if censored)
 # group = Binary covariate
 
-# For the interval censored model, time cannot start on exactly 0
+# For the interval censored model, time cannot start at exactly 0
 data$time <- ifelse(data$time == 0, data$time+0.01, 
                     data$time)
 
 # Make the data in the short format 
 data.id <-  data[!rev(duplicated(rev(data$id))),] 
 
-# Get the event rate for the  data set
+# Get the raw event rate for the  data set
 table(data.id$event)[2] / nrow(data.id)
 
 # Plot the data
-xyplot(y ~ time | event, group=id, data = data, type = "l")
+xyplot(y_a ~ time | event, group=id, data = data, type = "l")
 
 
 
 ##############################################
 ##     Fit the time-dependent Cox model     ##
 ##############################################
-##       	 Use y as biomarker   	        ##
+##       	 Use y_a as biomarker             ##
 ##############################################
 
 
 # TD-Cox model
-TD.Cox <- (coxph((Surv(time, time2 , event) ~ y + group + 
+TD.Cox <- (coxph((Surv(time, time2 , event) ~ y_a + group + 
                     cluster(id)), data = data))
 
 summary(TD.Cox)
 
 
 ########################################
-##      Fit the Basic Joint Model     ##
+##      Fit the basic joint model     ##
 ########################################
-##        Use y as biomarker          ##
+##        Use y_a as biomarker        ##
 ########################################
 
 # Fit the survival model
@@ -64,7 +67,7 @@ Surv <- coxph(Surv(Time, event) ~ group,
               data = data.id, x = TRUE, model = TRUE)
 
 # Fit the mixed model
-multMixedFit <- mvglmer(list(y ~ ns(time, knots = c(2,10)) + group  +
+multMixedFit <- mvglmer(list(y_a ~ ns(time, knots = c(2,10)) + group  +
                                (ns(time, knots = c(2,10)) | id)), 
                         data = data, families = list(gaussian))
 
@@ -80,12 +83,12 @@ exp(summary(JM1)$Survival)[,c(1,4,5)]
 
 
 
-########################################
-##      Fit the Basic Joint Model     ##
-########################################
-##          Use y as biomarker        ##
-##      Fit the model with IC data    ##
-########################################
+######################################################
+##      	    Fit the basic joint model 	          ##
+##  Accounting for interval censoring of the events ##
+######################################################
+##          	Use y_a as biomarker    	            ##
+######################################################
 
 # Fit the survival model with IC data
 SurvInt <- survreg(Surv(time, time2, event,  type = "interval") ~ group, 
@@ -93,7 +96,7 @@ SurvInt <- survreg(Surv(time, time2, event,  type = "interval") ~ group,
 summary(SurvInt)
 
 # Fit the mixed model
-multMixedFit <- mvglmer(list(y ~ ns(time, knots = c(2,10)) + group  +
+multMixedFit <- mvglmer(list(y_a ~ ns(time, knots = c(2,10)) + group  +
                                (ns(time, knots = c(2,10)) | id)), 
                         data = data, families = list(gaussian))
 
@@ -110,18 +113,20 @@ exp(summary(JM1.IC)$Survival)[,c(1,4,5)]
 
 
 ############################################
-##            Fit the JM model            ##
-##  Use slope as additional association   ##
+##            Fit the joint model         ##
+##  Use slope as additional covariate     ##
 ############################################
-##          Use y as biomarker            ##
+##          Use y_a as biomarker          ##
 ############################################
 
-Forms <- list("y" = "value",
-              "y" = list(fixed = ~ 0 + dns(time, knots = c(2,10)), 
-                         indFixed = c(2:4) , 
-                         random = ~ 0 + dns(time, knots = c(2,10)),
-                         indRandom = 2:4, name = "slope"))
-#Fit the second joint model
+# Define the associations: “value” and “slope”
+Forms <- list("y_a" = "value",
+              "y_a" = list(fixed = ~ 0 + dns(time, knots = c(2,10)), 
+                           indFixed = c(2:4) , 
+                           random = ~ 0 + dns(time, knots = c(2,10)),
+                           indRandom = 2:4, name = "slope"))
+
+# Fit the second joint model
 JM2 <- update(JM1, Formulas = Forms)
 
 # Inspect the traceplots
@@ -134,16 +139,16 @@ exp(summary(JM2)$Survival)[,c(1,4,5)]
 
 
 ############################################
-##            Fit the JM model            ##
+##          Fit the joint model           ##
 ##           Multimarker Model            ##
 ############################################
-##              Use y and y2              ##
+##  	       Use y_a and y_b              ##
 ############################################
 
 # Fit the mixed model for two markers
-multMixedFit2 <- mvglmer(list(y ~ ns(time, knots = c(2,10)) + group + 
+multMixedFit2 <- mvglmer(list(y_a ~ ns(time, knots = c(2,10)) + group + 
                                 (ns(time, knots = c(2,10)) | id),
-                              y2 ~ ns(time, knots = c(2,10)) + group + 
+                              y_b ~ ns(time, knots = c(2,10)) + group + 
                                 (ns(time, knots = c(2,10)) | id)), 
                          data = data, families = list(gaussian, gaussian))
 
@@ -161,11 +166,11 @@ exp(summary(JM3)$Survival)[,c(1,4,5)]
 
 
 ###########################################
-##      Fit the dynamic predictions      ##
+##      Make the dynamic predictions     ##
 ###########################################
 
-# Make a data set for a specific patient A
-NDA <- data[data$id == 28,] 
+# Make a data set for a specific patient A with the event
+NDA <- data[data$id == 330,] 
 
 # Estimate survival probabilities for different time points
 survPredsA <- vector("list", nrow(NDA))
@@ -173,11 +178,21 @@ for (i in 1:nrow(NDA)){
   survPredsA[[i]] <- survfitJM(JM1, newdata = NDA[1:i,], idVar = "id")
 }
 
-# Plot the graph on four different time points
-par(mfrow=c(2,2))
-for (i in c(1,3,5,6)) {
-  plot(survPredsA[[i]], estimator = "mean", conf.int = TRUE, 
-       fill.area = TRUE, col.area = "lightgrey", col.abline = "black", 
-       pch = 21, add.last.time.axis.tick = FALSE, include.y = TRUE, 
-       main = NULL, ylab = "", xlim = c(0,25))
+# Plot the graphs at four different time points
+for (i in c(1,3,5,8)) {
+  plot(survPredsA[[i]], ylab = "")
+}
+
+# Make a data set for a specific patient B without the event
+NDB <- data[data$id == 253,] 
+
+# Estimate survival probabilities for different time points
+survPredsB <- vector("list", nrow(NDB))
+for (i in 1:nrow(NDB)){
+  survPredsB[[i]] <- survfitJM(JM1, newdata = NDB[1:i,], idVar = "id")
+}
+
+# Plot the graphs at four different time points
+for (i in c(1,3,5,7)) {
+  plot(survPredsB[[i]], ylab = "")
 }
